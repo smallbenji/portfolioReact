7850c6be23fe51845eb827e771c7c328a444c9a2
Smallbenji <smallbenji@Windexe.localdomain>
2022-08-22 14:15:50 +0200

    yes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import "./App.scss";
 import FrontPage from "./components/frontpage/FrontPage";
-import TopBar from "./components/topbar/TopBar";
+
 
 function App() {
 	return (
 		<div className="App">
-			<TopBar />
 			<header className="App-header">
 				<FrontPage />
 			</header>
---
+++ src/components/frontpage/FrontPage.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './frontpage.scss';
+
+export default function FrontPage() {
+  return (
+    <div className='FrontPage'>
+        <div className='left'>
+            <img src="./assets/mig.jpg" className='selfie' />
+        </div>
+        
+        <div className='right'>
+
+        <p>
+            Hello there!
+        </p>
+
+        </div>
+
+    </div>
+  )
+}
